361df26ffb6722f5e60945fd7d155c5305c0b834
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2023-04-03 23:57:38 +0200

    Added test for containsColumn()

--- rf_cpp/tests/tests.cpp
+++ rf_cpp/tests/tests.cpp
@@ -30,6 +30,29 @@ TEST_CASE("DataFrame.getColumn returns correct value", "[getColumn]") {
     REQUIRE(some_floats == dataFrame.getColumn<double>("some_float").data());
 }
 
+TEST_CASE("DataFrame.containsColumn returns correct value", "[containsColumn]") {
+    std::vector<std::string> names = {"John", "Jane", "Josh"};
+    Column<std::string> names_column = Column<std::string>(names);
+    column_t col1 = &names_column;
+    std::vector<long> ages = {34, 35, 20};
+    Column<long> age_column = Column<long>(ages);
+    column_t col2 = &age_column;
+    std::vector<double> some_floats = {4.5, 5.0, 3.5};
+    Column<double> some_floats_column = Column<double>(some_floats);
+    column_t col3 = &some_floats_column;
+
+    std::vector<std::string> row_labels = {"1", "2", "3"};
+    std::vector<std::string> column_labels = {"name", "age", "some_float"};
+    std::vector<column_t> columns = {&names_column, &age_column, &some_floats_column};
+
+    DataFrame dataFrame = DataFrame(row_labels, column_labels, columns);
+
+    REQUIRE(dataFrame.containsColumn("name") == true);
+    REQUIRE(dataFrame.containsColumn("age") == true);
+    REQUIRE(dataFrame.containsColumn("some_float") == true);
+    REQUIRE(dataFrame.containsColumn("some_non-existent_column") == false);
+}
+
 // Column test cases
 
 TEST_CASE("Column.mean() returns correct value for integers", "[Column.mean()]") {
