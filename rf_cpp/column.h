5d531b257acdc7f06a04a5d5efa01be45a04d75b
Piotr <piotr.bledowski.77@gmail.com>
2023-04-05 14:21:40 +0200

    Added median(), min() and max() methods to Column class. Created corresponding tests for numeric data.

--- rf_cpp/column.h
+++ rf_cpp/column.h
@@ -20,9 +20,12 @@ template <typename T>
 class Column {
 private:
     std::vector<T> cells_;
+    std::vector<T> sorted_cells_;
 public:
     Column(std::vector<T> col_data) {
         cells_ = col_data;
+        sorted_cells_ = cells_;
+        std::sort(sorted_cells_.begin(), sorted_cells_.end());
     }
 
     std::vector<T> data() {
@@ -43,9 +46,21 @@ public:
         return std::accumulate(cells_.begin(), cells_.end(), 0.0) / (double) cells_.size();
     }
 
-//    double median() {
-//
-//    }
+    double median() {
+        if (std::is_same<T, std::string>::value || std::is_same<T, bool>::value)
+            throw std::runtime_error("Cannot call mean() for non-numeric columns!");
+        if (sorted_cells_.size() % 2 == 0)
+            return (double) (sorted_cells_[sorted_cells_.size() / 2 - 1] + sorted_cells_[sorted_cells_.size() / 2]) / 2.0;
+        return sorted_cells_[sorted_cells_.size() / 2];
+    }
+
+    T max() {
+        return sorted_cells_[sorted_cells_.size() - 1];
+    }
+
+    T min() {
+        return sorted_cells_[0];
+    }
 
     bool operator == (const Column &col) {
         for (int i = 0; i < cells_.size(); i++) {
--- rf_cpp/tests/tests.cpp
+++ rf_cpp/tests/tests.cpp
@@ -4,7 +4,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "../dataFrame.h"
-#include <limits> // standard-defined value of epsilon
+#include <limits> // standard-defined value of epsilon for float comparisons
 
 // DataFrame test cases
 
@@ -30,9 +30,32 @@ TEST_CASE("DataFrame.getColumn returns correct value", "[getColumn]") {
     REQUIRE(some_floats == dataFrame.getColumn<double>("some_float").data());
 }
 
+TEST_CASE("DataFrame.containsColumn returns correct value", "[containsColumn]") {
+    std::vector<std::string> names = {"John", "Jane", "Josh"};
+    Column<std::string> names_column = Column<std::string>(names);
+    column_t col1 = &names_column;
+    std::vector<long> ages = {34, 35, 20};
+    Column<long> age_column = Column<long>(ages);
+    column_t col2 = &age_column;
+    std::vector<double> some_floats = {4.5, 5.0, 3.5};
+    Column<double> some_floats_column = Column<double>(some_floats);
+    column_t col3 = &some_floats_column;
+
+    std::vector<std::string> row_labels = {"1", "2", "3"};
+    std::vector<std::string> column_labels = {"name", "age", "some_float"};
+    std::vector<column_t> columns = {&names_column, &age_column, &some_floats_column};
+
+    DataFrame dataFrame = DataFrame(row_labels, column_labels, columns);
+
+    REQUIRE(dataFrame.containsColumn("name") == true);
+    REQUIRE(dataFrame.containsColumn("age") == true);
+    REQUIRE(dataFrame.containsColumn("some_float") == true);
+    REQUIRE(dataFrame.containsColumn("some_non-existent_column") == false);
+}
+
 // Column test cases
 
-TEST_CASE("Column.mean() returns correct value for integer values", "[Column.mean()]") {
+TEST_CASE("Column.mean() returns correct value for integers", "[Column.mean()]") {
     std::vector<long> nums = {10, 15, 12, 13, 9, 16};
     double mean = 12.5;
     Column<long> nums_column = Column<long>(nums);
@@ -40,15 +63,46 @@ TEST_CASE("Column.mean() returns correct value for integer values", "[Column.mea
     REQUIRE(std::abs(mean - nums_column.mean()) < std::numeric_limits<double>::epsilon());
 }
 
-TEST_CASE("Column.mean() returns correct value for float values", "[Column.mean()]") {
-    std::vector<double> nums = {235.345, 15.34, 12.73, 13.0, 54234.234, 16.67, 7.777777};
-    double mean = 7790.728111;
+TEST_CASE("Column.mean() returns correct value for floats", "[Column.mean()]") {
+    std::vector<double> nums = {4.4, 6.66, 8.888, 2.0};
+    double mean = 5.487;
     Column<double> nums_column = Column<double>(nums);
-    double col_mean = nums_column.mean();
 
     REQUIRE(std::abs(mean - nums_column.mean()) < std::numeric_limits<double>::epsilon());
 }
 
-//TEST_CASE("DataFrame.getAllColumns returns correct value", "[getAllColumns]") {
-//
-//}
+TEST_CASE("Column.median() returns correct value for integers and odd length", "[Column.median()]") {
+    std::vector<long> nums = {345, 23, 76, 6, 9, 96, 123, 666, 777};
+    double median = 96.0;
+    Column<long> nums_column = Column<long>(nums);
+
+    REQUIRE(median == nums_column.median());
+}
+
+TEST_CASE("Column.median() returns correct value for floats and even length", "[Column.median()]") {
+    std::vector<double> nums = {65.34, 45.4, 76.15, 6.6, 9999.99324, 96.43, 123.123, 777.0};
+    double median = 86.29;
+    Column<double> nums_column = Column<double>(nums);
+
+    REQUIRE(std::abs(median - nums_column.median()) < std::numeric_limits<double>::epsilon());
+}
+
+TEST_CASE("Column.min() and Column.max() return correct value for integers", "[Column.min(), Column.max()]") {
+    std::vector<long> nums = {5, 2, 567, 345, 34, 76, 234, 65, 94, 27, 34578, 835, 74923467};
+    long min_val = 2;
+    long max_val = 74923467;
+    Column<long> nums_column = Column<long>(nums);
+
+    REQUIRE(min_val == nums_column.min());
+    REQUIRE(max_val == nums_column.max());
+}
+
+TEST_CASE("Column.min() and Column.max() return correct value for floats", "[Column.min(), Column.max()]") {
+    std::vector<double> nums = {5.6, 2.3, 9.21, 7.2};
+    double min_val = 2.3;
+    double max_val = 9.21;
+    Column<double> nums_column = Column<double>(nums);
+
+    REQUIRE(std::abs(min_val - nums_column.min()) < std::numeric_limits<double>::epsilon());
+    REQUIRE(std::abs(max_val - nums_column.max()) < std::numeric_limits<double>::epsilon());
+}
