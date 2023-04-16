da0b43b1ece4e25514731217928005a7a5d0de75
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2023-04-16 23:17:31 +0200

    Added expression functions for comparisons, so that expressions can be passed to functions as parameters

--- rf_cpp/utils/expressions.h
+++ rf_cpp/utils/expressions.h
@@ -6,3 +6,28 @@
 #define RF_CPP_EXPRESSIONS_H
 
 #endif //RF_CPP_EXPRESSIONS_H
+
+template <typename T>
+bool greater_than(T x, T y) {
+    return x > y;
+}
+
+template <typename T>
+bool less_than(T x, T y) {
+    return x < y;
+}
+
+template <typename T>
+bool greater_equal(T x, T y) {
+    return x >= y;
+}
+
+template <typename T>
+bool less_equal(T x, T y) {
+    return x <= y;
+}
+
+template <typename T>
+bool equal_to(T x, T y) {
+    return x == y;
+}
